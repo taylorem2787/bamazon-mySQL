1eae6510077beb6cc6a152efa03df7f2c03db25f
Taylor Emolo <tayloremolo@tay.local>
2016-10-27 12:43:10 -0400

    Adjustments

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,5 @@
 //Manager View (Next Level)
-
 // * Create a new Node application called `BamazonManager.js`. Running this application will:
-
 // 	* List a set of menu options: 
 // 		* View Products for Sale 
 // 		* View Low Inventory
@@ -9,11 +7,8 @@
 // 		* Add New Product
 
 // 	* If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
-
 // 	* If a manager selects `View Low Inventory`, then it should list all items with a inventory count lower than five.
-
 // 	* If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store. 
-
 // 	* If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
 //var prompt = require('prompt')
 var mysql 	 = require('mysql');
@@ -32,40 +27,43 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    manageInv();
 })
 
-connection.query('SELECT * FROM Products', function(err, res) {
-		console.log('|===============================================|');
-		console.log('|============ BAMAZON MANAGER VIEW =============|');
-		console.log('|===============================================|');
-    for (var i = 0; i < res.length; i++) {
-        console.log('|' + res[i].Product_Id + '  ' + res[i].Product_Name + "		|	" + res[i].Department_Name + " 		| " + res[i].Price + " | " + res[i].Stock_Quantity);
-    }
-    console.log("-----------------------------------");
-    runSearch();
-})
+// connection.query('SELECT * FROM Products', function(err, res) {
+// 		console.log('|===============================================|');
+// 		console.log('|============ BAMAZON MANAGER VIEW =============|');
+// 		console.log('|===============================================|');
+//     for (var i = 0; i < res.length; i++) {
+//         console.log('|' + res[i].Product_Id + '		' + res[i].Product_Name + "		|	" + res[i].Department_Name + " 		| " + res[i].Price + " | " + res[i].Stock_Quantity);
+//     }
+//     console.log("-----------------------------------");
+//     menuOptions();
+// })
 
-var runSearch = function() {
+function manageInv() {
+	productsView = [];
+	lowInvenView = [];
     inquirer.prompt({
         name: "InventoryMenu",
         type: "list",
         message: "Choose from the Inventory Menu options",
-        choices: ["sale", "loss", "add", "add product"]
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
     }).then(function(answer) {
         switch(answer.InventoryMenu) {
-            case "sale":
+            case "View Products for Sale":
                 viewSale();
             break;
 
-            case '2. VIEW LOW INVENTORY':
+            case "View Low Inventory":
                 viewLowI();
             break;
 
-            case '3. ADD TO INVENTORY':
+            case "Add to Inventory":
                 addInvy();
             break;
 
-            case '4. ADD NEW PRODUCT':
+            case "Add New Product":
                 addProd();
             break;
             default:
@@ -73,22 +71,63 @@ var runSearch = function() {
         }
     })
 };
+//Go back to main menu
+function mainMenu(){
+	inquirer.prompt({
+		name: "goBack",
+		type: "list",
+		message: "Return to main menu?",
+		choices: ['Yes', 'No']
+	}).then(function(answer){
+		if (answer.goBack == 'Yes'){
+			manageInv();
+		} else {
+			connection.end();
+		}
+	})
+};
 
 
-// var viewSale = function() {
-//         var query = 'SELECT * FROM Products WHERE ?'
-//         connection.query(query, {sale: answer.Products}, function(err, res) {
-//             for (var i = 0; i < res.length; i++) {
-//                 console.log("Position: " + res[i].Product_Id + " || Song: " + res[i].Product_Name + " || Year: " + res[i].Department_Name);
-//             }
-//             runSearch();
-//         })
-// };
+function viewSale(){
+	connection.query('SELECT * FROM Products', function(err, res) {
+    		console.log('|=====================================================================|');
+			console.log('|====================== BAMAZON INVENTORY VIEW =======================|');
+			console.log("|---------------------------------------------------------------------|");
+			console.log('|ID       PRODUCT  	    | DEPARTMENT   | PRICE  | STOCK QUANTITY  |');
+			console.log("|---------------------------------------------------------------------|");
+	    
+	    for (var i = 0; i<res.length; i++) {
+	        console.log('|' + res[i].Product_Id + '	  ' + res[i].Product_Name + " 	    | " + res[i].Department_Name + " 		  | " + res[i].Price + " | " + res[i].Stock_Quantity + " | ");
+	    	productsView.push(res[i]);
+	    	console.log("|---------------------------------------------------------------------|");
+	    }	
+	mainMenu();
+	})
+};
+
+function viewLowI(){
+	var query = 'SELECT * FROM Products WHERE Stock_Quantity < 5';
+	connection.query(query, function(err, res) {
+    		console.log('|=====================================================================|');
+			console.log('|====================== BAMAZON INVENTORY VIEW =======================|');
+			console.log("|---------------------------------------------------------------------|");
+			console.log('|ID     | PRODUCT  	  | DEPARTMENT   | PRICE  | STOCK QUANTITY |');
+			console.log("|---------------------------------------------------------------------|");
+       
+		for (var i = 0; i<res.length; i++) {
+			console.log('|' + res[i].Product_Id + '	  ' + res[i].Product_Name + " 	  | " + res[i].Department_Name + " 	  |" + res[i].Price + " 	| " + res[i].Stock_Quantity + " 	| ");
+			lowInvenView.push(res[i]);
+			console.log("|---------------------------------------------------------------------|");
+		}
+				//console.log("------------------------------------------------");
+	mainMenu();
+	})
+};
 
 
 			//console.log('|' + res[i].Product_Id + '	| ' + res[i].Product_Name + ' 	| $' + res[i].Department_Name + ' | ' + res[i].Price + '|' + res[i].Stock_Quantity + '	    |');
 	
 		//console.log('|___________________________________|');
-    	//runSearch();
+    	//menuOptions();
     
  
--- bamazon_customer.sql
+++ bamazon_customer.sql
@@ -11,25 +11,25 @@ CREATE TABLE Products(
 	PRIMARY KEY(Product_Id)
 );
 
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Shampoo ", "Beauty & Care", 10 , 80 );
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Conditioner ", "Beauty & Care", 12 , 100);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Lotion ", "Beauty & Care", 20 , 150);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Perfume ", "Beauty & Care", 50 , 50 );
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Shampoo", "Beauty & Care", 10, 80);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Conditioner","Beauty & Care", 12, 100);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Lotion", "Beauty & Care", 20, 150);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Perfume", "Beauty & Care", 50, 4);
 
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Shirt ", "Clothing ", 20 , 40);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Sweater ", "Clothing ", 18 , 50);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Jeans ", "Clothing ", 30 , 60);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Jacket ", "Clothing ", 35 , 70);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Shirt ", "Clothing", 20, 40);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Sweater ", "Clothing", 18, 50);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Jeans ", "Clothing", 30, 60);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Jacket ", "Clothing", 35, 70);
  
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Computer ", "Electronics	", 999, 30);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("iPad ", "Electronics	", 699, 30);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Camera ", "Electronics	", 300, 25);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Smart TV ", "Electronics	", 899, 50);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Computer ", "Electronics", 999, 30);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("iPad ", "Electronics", 699, 30);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Camera ", "Electronics", 300, 25);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Smart TV ", "Electronics", 899, 50);
 
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Coffee Maker", "Appliances	", 89 , 30);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Blender ", "Appliances	", 99 , 30);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Toaster ", "Appliances	", 49 , 25);
-INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Microwave ", "Appliances	", 199, 50);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Coffee Maker","Appliances", 89, 1);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Blender ", "Appliances", 99, 30);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Toaster ", "Appliances", 49, 25);
+INSERT INTO Products(Product_Name, Department_Name, Price, Stock_Quantity) value("Microwave ", "Appliances", 199, 50);
 
 DROP TABLE Products
 SELECT * FROM Products;
